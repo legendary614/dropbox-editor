e5bdf221bcbfcd3f818800e1b8f33dd9691c1960
legendary614 <36903047+legendary614@users.noreply.github.com>
2018-09-18 20:25:55 +0300

    first commit

---
+++ jquery-comments/data/comments-data.js
@@ -0,0 +1,71 @@
+var commentsArray = [
+// {
+//    "id": 1,
+//    "parent": null,
+//    "created": "2015-01-01",
+//    "modified": "2015-01-01",
+//    "content": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed posuere interdum sem.",
+//    "pings": [],
+//    "creator": 1,
+//    "fullname": "Simon Powell",
+//    "profile_picture_url": "https://i.imgur.com/V45GehK.jpg",
+//    "created_by_admin": false,
+//    "created_by_current_user": false,
+//    "upvote_count": 3,
+//    "user_has_upvoted": false,
+//    "is_new": false
+// },
+// {
+//    "id": 2,
+//    "parent": null,
+//    "created": "2015-01-02",
+//    "modified": "2015-01-02",
+//    "content": "Sed posuere interdum sem. Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien. Mauris varius diam vitae arcu.",
+//    "pings": [],
+//    "creator": 2,
+//    "fullname": "Jack Hemsworth",
+//    "profile_picture_url": "https://i.imgur.com/AS79XVr.jpg",
+//    "created_by_admin": false,
+//    "created_by_current_user": false,
+//    "upvote_count": 2,
+//    "user_has_upvoted": false,
+//    "is_new": false
+// },
+// {
+//    "id": 3,
+//    "parent": null,
+//    "created": "2015-01-03",
+//    "modified": "2015-01-03",
+//    "content": "@Hank Smith sed posuere interdum sem.\nQuisque ligula eros ullamcorper.",
+//    "pings": [],
+//    "creator": 3,
+//    "fullname": "Hank Smith",
+//    "profile_picture_url": "https://i.imgur.com/EW8v5yG.jpg",
+//    "created_by_admin": false,
+//    "created_by_current_user": false,
+//    "upvote_count": 2,
+//    "user_has_upvoted": true,
+//    "is_new": false
+// }
+]
+
+var usersArray = [
+   {
+      id: 1,
+      fullname: "Simon Powell",
+      email: "current.user@viima.com",
+      profile_picture_url: "https://i.imgur.com/V45GehK.jpg"
+   },
+   {
+      id: 2,
+      fullname: "Jack Hemsworth",
+      email: "jack.hemsworth@viima.com",
+      profile_picture_url: "https://i.imgur.com/AS79XVr.jpg"
+   },
+   {
+      id: 3,
+      fullname: "Hank Smith",
+      email: "hank.smith@viima.com",
+      profile_picture_url: "https://i.imgur.com/EW8v5yG.jpg"
+   }
+]
